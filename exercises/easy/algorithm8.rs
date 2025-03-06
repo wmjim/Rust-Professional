bf07f34a1c9ba74fe09024d0e43760e3ae9f1260
meng.wang <meng.w1016@outlook.com>
2025-03-06 19:00:03 +0800

    feat(algorithm8): 使用队列来实现栈的方法

--- exercises/easy/algorithm8.rs
+++ exercises/easy/algorithm8.rs
@@ -54,28 +54,53 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
 	q1:Queue<T>,
 	q2:Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        if self.q1.is_empty() {
+            self.q2.enqueue(elem);
+        } else {
+            self.q1.enqueue(elem);
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.q1.is_empty() && self.q2.is_empty() {
+            return Err("Stack is empty");
+        }
+
+        let (source, target) = if self.q1.is_empty() {
+            (&mut self.q2, &mut self.q1)
+        } else {
+            (&mut self.q1, &mut self.q2)
+        };
+
+        while source.size() > 1 {
+            if let Ok(value) = source.dequeue() {
+                target.enqueue(value);
+            }
+        }
+
+        source.dequeue()
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.q1.is_empty() && self.q2.is_empty()
+    }
+}
+
+impl <T> Default for myStack<T> {
+    fn default() -> myStack<T> {
+        myStack {
+            q1:Queue::<T>::new(),
+			q2:Queue::<T>::new()
+        }
     }
 }
 
